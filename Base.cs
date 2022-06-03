be5273f1a7a9962937129e81b21e221824f90efe
Roads-To-The-Doors <dimacernovolov@gmail.com>
2022-06-03 20:12:23 +0300

    Добавьте файлы проекта.

---
+++ Base.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Console_Database_Management_System
+{
+    class Base
+    {
+        // Добавить строку
+        public static void AddNote(string fileName)
+        {
+            if (System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                for (int i = 1; i > 0; ++i)
+                {
+                    Console.Write("\n Admin.Add_note.Line_to_write> ");
+                    
+                    string line = Convert.ToString(Console.ReadLine());
+
+                    char[] phrase = line.ToCharArray();
+
+                    if (phrase.Length >= 2 && phrase[phrase.Length - 2] == '\\' && phrase[phrase.Length - 1] == '0')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        StreamWriter writer = new System.IO.StreamWriter(GetAllPath(fileName), true);
+
+                        writer.WriteLine(line);
+
+                        writer.Close();
+                    }
+                }
+
+                Console.WriteLine("\n      Entries have been added.");
+            }
+            else
+            {
+                Console.WriteLine("\n       The file does not exist");
+            }
+        }
+
+        // Вывести строки
+        public static void OutNotes(string fileName)
+        {
+            if (System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                StreamReader sr = new StreamReader(GetAllPath(fileName));
+
+                string line;
+
+                int i = 0;
+
+                Console.WriteLine();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine($" {i}.        {line}");
+
+                    ++i;
+                }
+
+                sr.Close();
+            }
+            else
+            {
+                Console.WriteLine("\n       The file does not exist");
+            }
+        }
+
+        // Замена строки
+        public static void ReplaceNote(string fileName)
+        {
+            if (System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                Console.WriteLine("\n       Enter the line number");
+
+                Console.Write("\n Admin.Replace.Number_line_to_replace> ");
+
+                int numLine = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n       Enter the line");
+
+                Console.Write("\n Admin.Replace.Line_to_replace> ");
+
+                string repline = Console.ReadLine();
+                
+                string[] lines = System.IO.File.ReadAllLines(GetAllPath(fileName));
+
+                if (numLine < 0 || numLine >= lines.Length)
+                {
+                    Console.WriteLine("\n       !Error!");
+
+                    //ReplaceNote(fileName);
+                }
+                else 
+                {
+                    lines[numLine] = repline;
+
+                    StreamWriter scr = new StreamWriter(GetAllPath(fileName));
+
+                    for (int i = 0; i < lines.Length; ++i)
+                    {
+                        scr.WriteLine(lines[i]);
+                    }
+
+                    scr.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n       The file does not exist");
+            }
+        }
+
+        // Удаление строки
+        public static void RemoveNote(string fileName)
+        {
+            if (System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                Console.WriteLine("\n       Enter the line number");
+
+                Console.Write("\n Admin.Remove_note.Number_line_to_remove> ");
+
+                int numLine = Convert.ToInt32(Console.ReadLine());
+
+                string[] lines = System.IO.File.ReadAllLines(GetAllPath(fileName));
+
+                if (numLine < 0 || numLine >= lines.Length)
+                {
+                    Console.WriteLine("\n       !Error!");
+
+                    //RemoveNote(fileName);
+                }
+                else
+                {
+                    if (numLine == lines.Length - 1)
+                    {
+                        StreamWriter sc = new StreamWriter(GetAllPath(fileName));
+
+                        for (int i = 0; i < lines.Length - 1; ++i)
+                        {
+                            sc.WriteLine(lines[i]);
+                        }
+
+                        sc.Close();
+
+                        Console.WriteLine("\n       The line has been deleted");
+                    }
+                    else
+                    {
+                        for (int i = numLine; i < lines.Length - 1; ++i)
+                        {
+                            lines[i] = lines[i + 1];
+                        }
+
+                        StreamWriter sc = new StreamWriter(GetAllPath(fileName));
+
+                        for (int i = 0; i < lines.Length - 1; ++i)
+                        {
+                            sc.WriteLine(lines[i]);
+                        }
+
+                        sc.Close();
+
+                        Console.WriteLine("\n       The line has been deleted");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n       The file does not exist");
+            }
+        }
+
+        // Создание базы
+        public static void CreateDataBase(string fileName)
+        {
+            if (!System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                FileStream sr = File.Create(GetAllPath(fileName));
+                
+                sr.Close();
+
+                Console.WriteLine($"\n       The file {fileName} was created successfully");
+            }
+            else
+            {
+                Console.WriteLine("\n       Such a file already exists!");
+            }
+        }
+
+        // Удаление базы
+        public static void DeleteDataBase(string fileName)
+        {
+            if (System.IO.File.Exists(GetAllPath(fileName)))
+            {
+                System.IO.File.Delete(GetAllPath(fileName));
+
+                Console.WriteLine($"\n       Base {fileName} was successfully deleted");
+            }
+            else
+            {
+                Console.WriteLine($"\n       There is no database named {fileName}");
+            }
+        }
+
+        // Показать все базы
+        public static void ShowDataBases()
+        {
+            Console.Write("\n   Databases: ");
+
+            string[] files = Directory.GetFiles(Program.dir);
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine();
+
+                foreach (string s in files)
+                {
+                    Console.Write("\n       ");
+
+                    for (int i = Program.dir.Length + 1; i < s.Length - 4; ++i)
+                    {
+                        Console.Write(s[i]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        // Получаем полный путь к файлу, введя его имя // Только для использования в функциях
+        private static string GetAllPath(string fileName)
+        {
+            return (Program.dir + "\\" + fileName + ".txt");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Console_Database_Management_System
+{
+    class Program
+    {
+        public static void DoCommand()
+        {
+            Console.Write("\n Admin> ");
+
+            string command = Convert.ToString(Console.ReadLine());
+
+            switch (command)
+            {
+                case "help":
+
+                    Console.WriteLine("\n       help - find out all the commands\n" +
+                                      "\n       add note - add a new entry to the database. To finish typing, type \\0 in the last line\n" +
+                                      "\n       out notes - outputs all the information from the file\n" +
+                                      "\n       replace note - replacing a line (by number) with a given one\n" +
+                                      "\n       delete db - moves the database to the trash (recoverable)\n" +
+                                      "\n       create db - creates a new database\n" +
+                                      "\n       show dbs - displays a list of all databases created by the user\n" +
+                                      "\n       exit - exit the program\n" +
+                                      "\n       uninstall - remove directory and files"
+                                      );
+
+                    DoCommand();
+
+                    break;
+
+                case "add note":
+
+                    Console.WriteLine("\n       Enter name database");
+
+                    Console.Write("\n Admin.Add_note.Name_database> ");
+
+                    string nameDB = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("\n       Enter need lines");
+
+                    Base.AddNote(nameDB);
+
+                    DoCommand();
+
+                    break;
+
+                case "out notes":
+
+                    Console.WriteLine("\n       Enter name database");
+
+                    Console.Write("\n Admin.Out_notes.Name_database> ");
+
+                    string nameDateB = Convert.ToString(Console.ReadLine());
+
+                    Base.OutNotes(nameDateB);
+
+                    DoCommand();
+
+                    break;
+
+                case "replace note":
+
+                    Console.WriteLine("\n       Enter name database");
+
+                    Console.Write("\n Admin.Replace_notes.Name_database> ");
+
+                    string nameDBS = Convert.ToString(Console.ReadLine());
+
+                    Base.ReplaceNote(nameDBS);
+
+                    DoCommand();
+
+                    break;
+
+                case "remove note":
+
+                    Console.WriteLine("\n       Enter name database");
+
+                    Console.Write("\n Admin.Remove_notes.Name_database> ");
+
+                    string namedatebase = Convert.ToString(Console.ReadLine());
+
+                    Base.RemoveNote(namedatebase);
+
+                    DoCommand();
+
+                    break;
+
+                case "create db":
+
+                    Console.WriteLine("\n       Enter name database");
+
+                    Console.Write("\n Admin.Create_DB.Name_database> ");
+
+                    string nameDBForCreate = Convert.ToString(Console.ReadLine());
+
+                    Base.CreateDataBase(nameDBForCreate);
+
+                    DoCommand();
+
+                    break;
+
+                case "delete db":
+
+                    Console.WriteLine("\n       Enter the name of the database you want to delete");
+
+                    Console.Write("\n Admin.Delete_db.Name_database> ");
+
+                    string nameDBForDelete = Convert.ToString(Console.ReadLine());
+
+                    Base.DeleteDataBase(nameDBForDelete);
+
+                    DoCommand();
+
+                    break;
+
+                case "show dbs":
+
+                    Base.ShowDataBases();
+
+                    DoCommand();
+
+                    break;
+
+                case "exit":
+
+                    break;
+
+                case "uninstall":
+
+                    Console.Write("\n       Do you really want to delete the working folder and all the files it contains? Y|N  Yes|No\n\n Uninstall.Yes|No> ");
+
+                    string confirmation = Convert.ToString(Console.ReadLine());
+
+                    switch (confirmation)
+                    {
+                        case "Yes":
+                        case "Y":
+
+                            Directory.Delete(dir, true);
+
+                            Console.Write("\n       Ok, remove files");
+
+                            Thread.Sleep(500);
+
+                            Console.Write(".");
+
+                            Thread.Sleep(500);
+
+                            Console.Write(".");
+
+                            Thread.Sleep(500);
+
+                            Console.Write(".");
+
+                            Thread.Sleep(500);
+
+                            Console.WriteLine("\n\n       Done");
+
+                            Thread.Sleep(500);
+
+                            Console.WriteLine("\n       Attention, the next time you start the folder will be created automatically!");
+
+                            break;
+
+                        case "No":
+                        case "N":
+
+                            Console.Write("\n       Would you like to continue working? Y|N  Yes|No\n\n Continue.Yes|No> ");
+
+                            string confirm = Convert.ToString(Console.ReadLine());
+
+                            switch (confirm)
+                            {
+                                case "Yes":
+                                case "Y":
+
+                                    DoCommand();
+
+                                    break;
+
+                                case "No":
+                                case "N":
+
+                                    break;
+
+                                default:
+
+                                    Console.WriteLine("\n       There is no such command, so we will continue, and if you want to exit, enter the \"exit\" command");
+
+                                    DoCommand();
+
+                                    break;
+                            }
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("\n       There is no such command, so we will continue, and if you want to exit, enter the \"exit\" command");
+
+                            DoCommand();
+
+                            break;
+                    }
+
+                    break;
+
+                default:
+
+                    Console.WriteLine($"\n      !Error! Command {command} not found");
+
+                    DoCommand();
+
+                    break; 
+            }
+        }
+
+        public const string dir = @"C:\\Program Files\\.My_DataBase";
+
+        static void Main()
+        {
+
+            // Создание папки
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+            if (!dirInfo.Exists)
+            {
+                //DirectoryInfo diInfo = Directory.CreateDirectory(dir);
+
+                dirInfo = Directory.CreateDirectory(dir);
+
+                dirInfo.Attributes = FileAttributes.Hidden;
+            }
+
+            Console.WriteLine("\n       Hi, this is a console management system for your own database");
+
+            Console.WriteLine("\n       Enter the command (for reference, enter \"help\")");
+
+            DoCommand();
+
+            Thread.Sleep(500);
+
+            Console.WriteLine("\n       GOOD LUCK!");
+
+            Thread.Sleep(500);
+
+            Console.ReadKey();
+        }
+    }
+}
